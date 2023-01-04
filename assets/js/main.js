fa1c3e2d828ce9ca387104c5f70733276d2e2a1f
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-01-04 15:33:57 +0100

    or

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,19 @@ let z = 20
 // x > z && x > y
         // 10 > 20 && 10 > 15 
 console.log( x > z && x > y ) // false
+        // falsch && falsch 
+
+
+// x != y 
+        // 10 != 20 
+console.log( x!= y ) // true
+        // true
+
+// z <= y || z >=x  
+        // 20 <= 15 or 20 >= 10 
+console.log(z <= y || z>=x) // true
+
+
+// x == z || x != y 
+// 10 == 20 or 10 != 15      false true    true
+console.log(x == z || x!= y) // true
